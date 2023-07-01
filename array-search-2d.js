793990af9cf0a627bbc6499e3fe8835a891a0250
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-07-01 11:21:09 -0700

    2D array search

---
+++ array-search-2d.js
@@ -0,0 +1,15 @@
+// Write code to create a function that accepts a two-dimensional array
+// Each 2D element contains either the string "X" or "O" (both capitalized)
+// Return the number of times "X" appears in the 2D array
+
+var arraySearch2D = function(arr) {
+    var xAppearence = 0;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] == "X") {
+                xAppearence++;
+            }
+        }
+    }
+    return xAppearence;
+};
